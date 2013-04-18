87b7eba721e9400e0d3641971fbfd0250e3c16e9
jasonhutchens <jason@agworld.com.au>
2013-04-18 23:01:28 +0800

    display guy

--- dwarf_squad.rb
+++ dwarf_squad.rb
@@ -6,6 +6,46 @@ require 'bundler'
 Bundler.require(:default)
 
 class Game < Chingu::Window
+
+  def initialize
+    super(800, 600, false)
+
+    self.input = {
+      escape: :exit
+    }
+
+    switch_game_state(Level)
+  end
+
+end
+
+class Level < Chingu::GameState
+
+  def initialize
+    super
+
+    @player = Player.create(x: 100, y: 100, zorder: 500)
+  end
+
+  def setup
+  end
+
+  def update
+    super
+  end
+
+  def draw
+    super
+  end
+
+end
+
+class Player < Chingu::GameObject
+
+  def setup
+    @image = Gosu::Image['captain.png']
+  end
+
 end
 
 if __FILE__ == $0
